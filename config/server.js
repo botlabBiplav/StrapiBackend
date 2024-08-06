0580e25b5b5208cac718057f1aa8c0919e3c590d
botlabBiplav <biplav.mazumdar@botlabdynamics.com>
2024-08-06 12:20:19 +0530

    Update server.js
    remove .env.array and add direct key

--- config/server.js
+++ config/server.js
@@ -2,7 +2,12 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('90EPcHfZk+JRXUbdPYEmcg==,7OnQVlelcETYZfBmJnTMIA==,H1/Xp/X/Z0y7rvYI+OM0Mg==,PcSszYdaz2QUwzIsRnjfoQ=='),
+      keys: [
+    '90EPcHfZk+JRXUbdPYEmcg==',
+    '7OnQVlelcETYZfBmJnTMIA==',
+    'H1/Xp/X/Z0y7rvYI+OM0Mg==',
+    'PcSszYdaz2QUwzIsRnjfoQ=='
+  ],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
