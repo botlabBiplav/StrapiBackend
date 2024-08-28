12ab91663102c4dc48fc952d715ca4126848c98b
BiplavBotLab <biplav.mazumdar@botlabdynamics.com>
2024-08-28 17:54:51 +0530

    postgres database added

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,26 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('US West (Oregon) us-west-2'),
+            params: {
+              Bucket: env('botlab-website'),
+            },
+          }
+        },
+        // These parameters could solve issues with ACL public-read access — see [this issue](https://github.com/strapi/strapi/issues/5868) for details
+        actionOptions: {
+          upload: {
+            ACL: null
+          },
+          uploadStream: {
+            ACL: null
+          },
+        }
+      },
+    }
+  });
